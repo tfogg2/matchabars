706ee753f8cb4f2a0a5763ff002b3f8771df59b8
Thomas Fogg <thomasfogg@Thomass-Mini.attlocal.net>
2020-08-23 02:07:30 -0500

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config()
 
 const app = express()
 
-app.use(express.json())
-app.use(cors())
+app.use(express.static(path.join(__dirname, "build")))
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+})
 
 const PORT = process.env.PORT || 5000
 
